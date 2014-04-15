99e8b1c65ed131a36c4e9888c73e6fbf5ead47e7
Maigen <m@skjelstad.org>
2014-04-14 18:45:49 -0700

    acceptance test for sign up, sign out, sign in workflow.

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 feature "Signup" do
 	
   scenario "Signing up as a new user" do
-  	visit('users')
-  	click_link('Sign Up')
+  	visit 'users'
+  	click_link 'Sign Up'
   	fill_in 'Name', :with => 'Emma'
     fill_in 'Email', :with => 'em@ma.com'
     fill_in 'Username', :with => 'emmadilemma'
@@ -15,5 +15,14 @@ feature "Signup" do
     fill_in 'Total budget amount', :with => '1000'
     click_button 'Create User'
   	page.should have_content 'Profile created. Welcome!'
+
+    click_link 'Log Out'
+    page.should have_content 'Logged out!'
+
+    click_link 'Log In' 
+    fill_in 'Email', :with => 'em@ma.com'
+    fill_in 'Password', :with => '1234'
+    click_button 'Log In'
+    page.should have_content 'Logged in!'
   end
 end
