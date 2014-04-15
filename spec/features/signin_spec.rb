45998048febe56d1158ffff2ec3abe54a0e2b281
Maigen <m@skjelstad.org>
2014-04-15 05:05:12 -0700

    adjusted layout on spec file

--- spec/features/signin_spec.rb
+++ spec/features/signin_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 feature "Signup" do
-	
+
   scenario "Signing up as a new user" do
-  	visit 'users'
-  	click_link 'Sign Up'
-  	fill_in 'Name', :with => 'Emma'
+    visit 'users'
+    click_link 'Sign Up'
+    fill_in 'Name', :with => 'Emma'
     fill_in 'Email', :with => 'em@ma.com'
     fill_in 'Username', :with => 'emmadilemma'
     fill_in 'Password', :with => '1234'
@@ -14,7 +14,7 @@ feature "Signup" do
     fill_in 'Role in wedding', :with => 'Bride'
     fill_in 'Total budget amount', :with => '1000'
     click_button 'Create User'
-  	page.should have_content 'Profile created. Welcome!'
+    page.should have_content 'Profile created. Welcome!'
 
     click_link 'Log Out'
     page.should have_content 'Logged out!'
