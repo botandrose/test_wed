57d7bf5eca72b07e2582b8a9cee3d59f85741b12
Maigen <m@skjelstad.org>
2014-05-12 16:35:55 -0700

    updated controller with new/create/show and strong params

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,10 +1,21 @@
 class BudgetsController < ApplicationController
 
 	def index
-	
+		@budget = Budget.new
 	end
 
 	def create
-		
+		@budget = Budget.create(budget_params)
+		redirect_to budget_path(@budget)
+	end
+
+	def show
+		@budget = Budget.find(params[:id])
+		@total_budget = @budget.calculate_budget
+	end
+
+	private
+	def budget_params
+		params.require(:budget).permit(:speculated_amount, :saved, :months_until, :monthly_savings)
 	end
 end
