d27582707c6cf8cd852713e4b7fc505e52cfb268
Maigen <m@skjelstad.org>
2014-05-12 13:37:51 -0700

    any changes made after previous commits. includes massive frustrations

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,10 +1,18 @@
 class BudgetsController < ApplicationController
 
 	def index
-	
+		@budget = Budget.new
 	end
 
 	def create
-		
+		@budget = Budget.create(budget_params)
+		render :show
 	end
+
+	private
+
+	def budget_params
+		params.require(:budget).permit(:speculated_amount, :saved, :months_until, :monthly_savings)
+	end
+
 end
--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,16 +1,16 @@
-class Budget 
+class Budget < ActiveRecord::Base
 
-	def initialize(speculated_amount, saved, months_until, monthly_savings)
-		@speculated_amount = speculated_amount.to_i
-		@saved = saved.to_i
-		@months_until = months_until.to_i
-		@monthly_savings = monthly_savings.to_i
-		@saved_before_wedding = @months_until * @monthly_savings
-	end
+	# def initialize(speculated_amount, saved, months_until, monthly_savings)
+	# 	@speculated_amount = speculated_amount.to_i
+	# 	@saved = saved.to_i
+	# 	@months_until = months_until.to_i
+	# 	@monthly_savings = monthly_savings.to_i
+	# 	@saved_before_wedding = @months_until * @monthly_savings
+	# end
 
-	def calculate_budget
-		@total_budget = (@saved_before_wedding.to_i + @saved).to_s
-	end
+	# def calculate_budget
+	# 	@total_budget = (@saved_before_wedding.to_i + @saved).to_s
+	# end
 
 	
 end
