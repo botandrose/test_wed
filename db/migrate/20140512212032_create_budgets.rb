7c6ec3263000de9ee463c28d1b296be319d6d193
Maigen <m@skjelstad.org>
2014-05-12 17:02:03 -0700

    budgets table

---
+++ db/migrate/20140512212032_create_budgets.rb
@@ -0,0 +1,12 @@
+class CreateBudgets < ActiveRecord::Migration
+  def change
+    create_table :budgets do |t|
+    	t.string :speculated_amount
+    	t.string :saved
+    	t.integer :months_until
+    	t.string :monthly_savings
+
+    	t.timestamps
+    end
+  end
+end
