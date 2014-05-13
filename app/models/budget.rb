e421f0814dd175fdc665ae0165759ecc56c9559e
Maigen <m@skjelstad.org>
2014-05-13 10:49:21 -0700

    validating amount saved

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,13 +1,13 @@
 class Budget < ActiveRecord::Base
 
 	validates :speculated_amount, presence: true
-	validates :saved, presence: true
+	validates :amount_saved, presence: true
 	validates :months_until, presence: true
 	validates :monthly_savings, presence: true
 
 	def calculate_budget
 		saved_before_wedding = (self.months_until.to_i * self.monthly_savings.to_i)
-		total_budget = (saved_before_wedding + self.saved.to_i).to_s
+		total_budget = (saved_before_wedding + self.amount_saved.to_i).to_s
 	end
 
 end
