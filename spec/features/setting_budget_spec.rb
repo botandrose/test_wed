1ba42f1537bbc750dcdab5266bbdda2bcb09da58
Maigen <m@skjelstad.org>
2014-05-12 17:53:41 -0700

    changed wording on existing tests and added testing of show page

--- spec/features/setting_budget_spec.rb
+++ spec/features/setting_budget_spec.rb
@@ -4,13 +4,13 @@ feature 'Setting a Total Budget' do
 
   scenario "Setting a Budget with One Contributor" do
     visit '/'
-    page.should have_content 'Setting Your Total Wedding Budget'
-    fill_in 'How much do you want to spend?', with: '10000'
-    page.should have_content '(if not sure, leave empty)'
-    fill_in 'How much do you already have saved?', with: '7000'
-    select 'Nine Months', from: 'Months Until Wedding'
-    page.should have_content '(This is how long you have to save. We recommend not including the final month prior to the actual wedding date.)'
+    page.should have_content 'Setting a Wedding Budget'
+    fill_in 'How much do you think you want to spend?', with: '10000'
+    fill_in 'How much have you already saved?', with: '7000'
+    fill_in 'How many months until the wedding?', with: '9'
     fill_in 'How much can you realistically set aside each month?', with: '200'
+    click_on 'What\'s my budget?'
+    page.should have_content 'Weddings are expensive. You should approach your wedding planning having set a budget that is within your means.'
     page.should have_content 'The national average for a wedding in 2013 was $25,200.'
     page.should have_content 'According to the information you have provided, your Total Wedding Budget is: $8,800'
   end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -19,3 +19,15 @@ def log_in(user)
   fill_in 'Password', with: user.password
   click_button 'Log In'
 end
+
+def create_budget(attributes={})
+  defaults = {
+    speculated_amount: "10000",
+    saved: "7000",
+    months_until: "9",
+    monthly_savings: "200"
+  }
+
+  attributes = defaults.merge(attributes)
+  Budget.create!(attributes)
+end
