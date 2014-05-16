6465bc23eb3ec02ae0f7af0eaaa9d70df2dc12ca
Maigen <m@skjelstad.org>
2014-05-16 13:46:48 -0700

    changed index file to include display of result (instead of separate show page)

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -5,19 +5,8 @@ class BudgetsController < ApplicationController
 	end
 
 	def create
-		@budget = Budget.create(budget_params)
-		redirect_to budget_path(@budget)
-	end
-
-	def show
-		@budget = Budget.find(params[:id])
-		@speculated_amount = @budget.speculated_amount
-		@total_budget = @budget.calculate_budget
-	end
-
-	private
-	def budget_params
-		params.require(:budget).permit(:speculated_amount, :amount_saved, :months_until, :monthly_savings)
+		@budget = Budget.new(params[:budget])
+		render :index
 	end
 
 end
