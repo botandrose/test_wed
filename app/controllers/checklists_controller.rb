a3caa58c3ab8676026e34b1d90a69457327bdaa3
Micah Geisel <micah@botandrose.com>
2014-05-20 13:36:52 -0700

    rm cruft.

--- app/controllers/checklists_controller.rb
+++
@@ -1,5 +0,0 @@
-class ChecklistsController < ApplicationController
-  def index
-    
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ TestWedding::Application.routes.draw do
   root 'budgets#index'
 
   resources :budgets
-  resources :checklists
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
